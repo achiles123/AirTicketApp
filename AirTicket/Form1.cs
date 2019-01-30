d74bdeef1d669f7b8d44cebfc1f9e394ae43e4c3
abc <nhk.nguyen1994@gmail.com>
2019-01-30 12:02:11 +0700

    a

--- AirTicket/Form1.cs
+++ AirTicket/Form1.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:629b4f0af22d2c9900340f6418dacaddf5aa63d28124baa44dddd8629974e13a
-size 6864
+oid sha256:c012afcc995185a9ed724f459b594d8f19b358c66a5adafd8de69b3e0a5fc877
+size 6958
---
+++ AirTicket/bin/x64/Debug/README.txt
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:6309ff37944dc213fe6487bf7d88610afa273771f701a7e73464b22bb9b6a568
+size 5900
